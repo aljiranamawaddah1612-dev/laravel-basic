4eb083849d3b358f16aae6496ec427964169964f
Aljirana Mawaddah <aljiranamawaddah1612-dev.com>
2026-04-03 09:02:56 +0700

    Pertemuan 2: Blade Template

---
+++ resources/views/Student/create.blade.php
@@ -0,0 +1,5 @@
+<x-app>
+    <x-slot:title> {{ $title}} </x-slot>
+
+        <h1 class="fw-bold">CREATE STUDENT</h1>
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Student', function () {
+    return view('Student.index', ['title' => ' STUDENT']);
+});
+
+Route::get('/Student/create', function () {
+    return view('Student.create', ['title' => ' CREATE STUDENT']);
+});
